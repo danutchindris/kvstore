3f43f261a381ed890e1050a7d488b6dfafff3889
Danut Chindris <danutchindris@gmail.com>
2025-10-02 08:16:48 +0300

    Implement various functions for the key-value store

--- src/kvstore.erl
+++ src/kvstore.erl
@@ -1,3 +1,21 @@
 -module(kvstore).
 
--export([]).
+-export([init/0, add/3, update/3, remove/2, get/2, keys/1]).
+
+init() ->
+    #{}.
+
+add(K, V, #{} = S) ->
+    S#{K => V}.
+
+update(K, V, S) ->
+    S#{K := V}.
+
+remove(K, S) ->
+    maps:remove(K, S).
+
+get(K, S) ->
+    maps:get(K, S).
+
+keys(S) ->
+    lists:sort(maps:keys(S)).
